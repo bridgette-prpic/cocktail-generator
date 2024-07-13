145014771e943b1c99e5474eda201674f0c9f1ee
bridgette-prpic <165822096+bridgette-prpic@users.noreply.github.com>
2024-07-13 12:51:10 +0200

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateCocktail(event) {
+  event.preventDefault();
 
+  new Typewriter("#cocktail", {
+    strings: ["Vodka, Dry Vermouth, Olive Brine, Blue Cheese Olives"],
+    autoStart: true,
+    delay: 30,
+    cursor: "",
+  });
+}
+
+let cocktailFormElement = document.querySelector("#cocktail-generator-form");
+cocktailFormElement.addEventListener("submit", generateCocktail);
