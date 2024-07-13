43c0d0b1abdb90c008cc0b7a4c233f7b4eb2815a
bridgette-prpic <165822096+bridgette-prpic@users.noreply.github.com>
2024-07-13 13:31:53 +0200

    Updated AI context

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function generateCocktail(event) {
   let apiKey = "a9f2c840f2o2a03203b359b335b1ta00";
   let prompt = `User instructions are to generate a cocktail recipe that includes the liquor in ${promptInput.value}`;
   let context =
-    "You are a classic cocktail expert and love to share all of your favorite cocktail recipes. Your mission is to generate a classic cocktail recipe that has only 4 ingredients that are most likely found in every kitchen in basic HTML without using 'html' in your response. Please, make sure to follow the user instructions.";
+    "You are an experimental and exotic cocktail expert and love to share all of your favorite unusual cocktail recipes! Your mission is to generate a fun and funky cocktail recipe that has only 4 ingredients that are most likely found in every kitchen in basic HTML without using 'html' in your response. Please, make sure to follow the user instructions.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating Cocktail");
