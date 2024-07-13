b5361a118ebb7c5b3715c88fa6441d89bad2b6d6
bridgette-prpic <165822096+bridgette-prpic@users.noreply.github.com>
2024-07-13 13:23:04 +0200

    Added API integration, added axios, created AI prompts and context, and more CSS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+function displayCocktail(response) {
+  console.log("cocktail generated");
+  new Typewriter("#cocktail", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 10,
+    cursor: "",
+  });
+}
+
 function generateCocktail(event) {
   event.preventDefault();
 
-  new Typewriter("#cocktail", {
-    strings: ["Vodka, Dry Vermouth, Olive Brine, Blue Cheese Olives"],
-    autoStart: true,
-    delay: 30,
-    cursor: "",
-  });
+  let promptInput = document.querySelector("#user-prompt");
+
+  let apiKey = "a9f2c840f2o2a03203b359b335b1ta00";
+  let prompt = `User instructions are to generate a cocktail recipe that includes the liquor in ${promptInput.value}`;
+  let context =
+    "You are a classic cocktail expert and love to share all of your favorite cocktail recipes. Your mission is to generate a classic cocktail recipe that has only 4 ingredients that are most likely found in every kitchen in basic HTML without using 'html' in your response. Please, make sure to follow the user instructions.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating Cocktail");
+  console.log(`prompt: ${prompt}`);
+  console.log(`prompt: ${context}`);
+  axios.get(apiUrl).then(displayCocktail);
 }
 
 let cocktailFormElement = document.querySelector("#cocktail-generator-form");
